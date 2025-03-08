12a21321338c4f897d0b8c01096104f78d4a1fcf
Max Alekseev <m.alekseev10@yandex.ru>
2025-03-08 12:22:33 +0300

    added change data user tests

---
+++ src/test/java/user/ChangeUserDataParameterizedTest.java
@@ -0,0 +1,62 @@
+package user;
+
+import client.StellarBurgersServiceClient;
+import com.github.javafaker.Faker;
+import io.qameta.allure.junit4.DisplayName;
+import io.restassured.response.ValidatableResponse;
+import model.User;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class ChangeUserDataParameterizedTest {
+
+    private final User changeDataUser;
+
+    private StellarBurgersServiceClient client;
+    private static final String baseURI = "https://stellarburgers.nomoreparties.site";
+    private String accessToken;
+
+
+    public ChangeUserDataParameterizedTest(User changeDataUser) {
+        this.changeDataUser = changeDataUser;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getDataTest() {
+        Faker faker = new Faker();
+        return new Object[][] {
+                {new User(faker.internet().emailAddress(), null, null)},
+                {new User(null, faker.bothify("#?#?#?#"), null)},
+                {new User(null, null, faker.name().firstName())},
+                {new User(faker.internet().emailAddress(), faker.bothify("#?#?#?#"), null)},
+                {new User(faker.internet().emailAddress(), null, faker.name().firstName())},
+                {new User(null, faker.bothify("#?#?#?#"), faker.name().firstName())}
+        };
+    }
+
+    @Before
+    public void prepareData() {
+        client = new StellarBurgersServiceClient(baseURI);
+    }
+
+    @Test
+    @DisplayName("Изменение данных пользователя с различным набором данных")
+    public void checkingChangingVariousUserDataSuccessful() {
+        Faker faker = new Faker();
+        User user = new User(faker.internet().emailAddress(), faker.bothify("#?#?#?#"), faker.name().firstName());
+        accessToken = client.createUserAndGetToken(user);
+
+        ValidatableResponse changeResponse = client.changeUser(accessToken, changeDataUser);
+        changeResponse.statusCode(200);
+    }
+
+    @After
+    public void dataCleanup() {
+        client.deleteUser(accessToken);
+    }
+
+}
